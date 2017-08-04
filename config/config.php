4718e5423bf765fefbb473bf9f196ba67c7f22dd
Tobias Schwarz <code@tobias-schwarz.me>
2017-08-04 19:30:51 +0000

    Adapts the new config system

--- config/config.php
+++ config/config.php
@@ -1,18 +1,19 @@
 <?php
-// $config['name'] = 'Clan2Columns';
-// $config['version'] = '0.0.1';
-// $config['author'] = 'Tobias Schwarz';
-// $config['link'] = 'http://ilch.de';
-// $config['desc'] = '2 Spalten Clan Layout';
-// // $config['layouts']['index_full'] = [['module' => 'user', 'controller' => 'panel']]; //only for example
-// // $config['layouts']['index_full'] = [['module' => 'forum']]; //only for example
-// //$config['layouts']['index_full'] = array(array('module' => 'guestbook', 'controller' => 'index', 'action' => 'newentry'));
-// //$config['modulekey'] = 'Name of Module';
 
-$config = [
-    'name' => "clan2columns",
-    'version' => '0.0.1',
-    'author' => 'Tobias Schwarz',
-    'link' => 'http://ilch.de',
-    'desc' => '2 Spalten Clan Layout',
-];
+namespace Layouts\Clan2Columns\Config;
+
+class Config extends \Ilch\Config\Install
+{
+    public $config = [
+        'name' => "clan2columns",
+        'version' => '0.0.1',
+        'author' => 'Tobias Schwarz',
+        'link' => 'http://ilch.de',
+        'desc' => '2 Spalten Clan Layout',
+    ];
+
+    public function getUpdate($installedVersion)
+    {
+
+    }
+}
