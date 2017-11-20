bbcc8fed2b963f26103cc4751a271fd0a0c7d9e7
Tobias Schwarz <code@tobias-schwarz.me>
2017-11-20 22:22:55 +0100

    Remove unnecessary code from webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,6 @@ mix.options({
  */
 
 mix.copyDirectory('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'fonts/bootstrap');
-// mix.copy('node_modules/font-awesome/fonts/*', 'fonts/');
 mix.sass('src/scss/app.scss', 'css/');
 mix.js('src/js/app.js', 'js/');
 
